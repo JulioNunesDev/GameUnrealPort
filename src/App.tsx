00d883c6171d82b1885a951c6c0b0eb9947ca7d6
Julio Nunes <julionunes999@gmail.com>
2023-10-11 04:24:56 -0300

    feat: coomponent Iframe3D

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,50 @@
 import gsap from "gsap";
-import  {useRef, useLayoutEffect, RefObject, } from 'react'
-
+import { useRef, useLayoutEffect, RefObject } from "react";
+import Iframe3D from "./Iframes3D/Iframe3D";
 
 function App() {
-
-
   const root: RefObject<HTMLDivElement> = useRef(null);
 
   useLayoutEffect(() => {
-    const ctx = gsap.timeline()
-      // all your animations go in here...
-      ctx.to(root.current, { rotation: "360", ease: 'all Linear .2s' });
-      ctx.from(root.current, {padding: 40, } );
-     
-  
+    const ctx = gsap.timeline();
+    // all your animations go in here...
+    ctx.to(root.current, { rotation: "360", ease: "all Linear .2s" });
+    ctx.from(root.current, { padding: 40 });
   }, []);
 
-
   return (
-    <div   style={{overflow: 'hidden',backgroundColor: "var(--color-night)", width: '100%', height: '100vh'}}>
-      <h1 
-     ref={root}
+    <div
+      style={{
+        overflow: "hidden",
+        backgroundColor: "var(--color-night)",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <div>
+        <Iframe3D/>
+      </div>
+      <h1
+        ref={root}
         style={{
           color: "var(--color-midnight)",
           fontSize: "var(--size-titleup)",
           fontFamily: "var(--font-Oxa)",
         }}
       >
-        Site em Desenvolvimento!
-        Libs: Presentes 
-        [
-        Green Sock,
-        Three Js
-        ]
+        Site em Desenvolvimento! Libs: Presentes [ Green Sock, Three Js ]
       </h1>
-      <a 
-      
+      <a
         style={{
-         color: "var(--color-midnight)",
+          color: "var(--color-midnight)",
           fontSize: "var(--size-title)",
           fontFamily: "var(--font-Oxa)",
         }}
         href="https://www.instagram.com/julionunesyt/"
         target="_blank"
-      >Julio Nunes - <span>click Aqui!</span></a>
+      >
+        Julio Nunes - <span>click Aqui!</span>
+      </a>
     </div>
   );
 }
---
+++ src/Iframes3D/Iframe3D.tsx
@@ -0,0 +1,32 @@
+export default function Iframe3D() {
+
+    type FullscreenObject = {
+        allowFullScreen: boolean;
+        mozallowfullscreen: boolean;
+        webkitallowfullscreen: boolean;
+        allow: string;
+    };
+    const MyFrame: FullscreenObject = {
+        allowFullScreen: true,
+        mozallowfullscreen: true,
+        webkitallowfullscreen: true,
+        allow:"autoplay",
+    }
+  return (
+    <div className="sketchfab-embed-wrapper">
+      {" "}
+      <iframe  {...MyFrame}
+        title="Alissa"
+        xr-spatial-tracking
+        execution-while-out-of-viewport
+        execution-while-not-rendered
+      
+        web-share
+        width='100%'
+        height='500px'
+        src="https://sketchfab.com/models/24aae01ca3a64a8c9dfc232840c2542b/embed"
+      ></iframe>
+    </div>
+  );
+
+}
